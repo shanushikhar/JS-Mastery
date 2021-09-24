6cee3f3683f82f9e462d05f56152182cdc771abc
Shanu Shikhar <thisisshanushikhar@gmail.com>
2021-09-24 15:44:10 +0530

    added DEcorated function

--- Frontend masters/Chaining.js
+++ Frontend masters/Chaining.js
@@ -19,15 +19,66 @@ console.log(Allval)
 // Doing with reducer
 
 const OwnReducer = (array, ourFn, accumulator) => {
-    for (let i = 0; i < array.length; i++){
-        accumulator = ourFn(array[i],accumulator)
+    for (let i = 0; i < array.length; i++) {
+        accumulator = ourFn(array[i], accumulator)
     }
     return accumulator
 }
 
-const CallFn = (fn,val) => {
-    return fn(val)
+const CallFn = (fn, val) => fn(val)
+
+const valOFReduc = OwnReducer([add, multiple, divide], CallFn, 15)
+console.log(valOFReduc)
+
+
+// Closure
+const functionCreator = () => {
+    const add = val => {
+        return val * 2
+    }
+    return add
+    // OR => Anonymous fn
+    // return val => {
+    //     return val * 2
+    // }
+
 }
 
-const valOFReduc = OwnReducer([add, multiple, divide], CallFn, 10)
-console.log(valOFReduc)
+const generatedFn = functionCreator()
+console.log(generatedFn(5))
+
+// another example
+const HelloClosure = () => {
+    let counter = 0;
+    const val = () => {
+        return counter
+        // return 5
+    }
+    return val
+}
+
+let myValClosure = HelloClosure()
+console.log(myValClosure())
+
+
+// Decorated function
+const octify = (calfn) => {
+    let counter = 0;
+    const val = (input) => {
+        if (counter === 0) {
+            counter++
+            let value = calfn(input)
+            return value
+        }
+        return "sorry"
+    }
+    return val
+}
+
+const mulby2 = (inp) => {
+    return inp * 2
+}
+
+const values = octify(mulby2)
+console.log(values(10))
+console.log(values(20))
